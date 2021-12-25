e6584c1129bc5850990cf648d59c25b5d74c1469
Quentin Pinçon <qpincon@masae-analytics.com>
2021-12-25 18:52:08 +0100

    first version of finding anagrams working

--- anagramdr/src/main.rs
+++ anagramdr/src/main.rs
@@ -1,17 +1,20 @@
-#[macro_use]
-extern crate lazy_static;
+// #[macro_use]
+// extern crate lazy_static;
 
+use std::time::Instant;
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
 use serde_json::{Value, Map};
-use std::collections::{HashMap, HashSet};
+use std::collections::HashMap;
+use std::str;
+use std::fmt;
 
 const MORPH_TYPES_LEN : usize = 3;
 const MORPH_TYPES : [&'static str; MORPH_TYPES_LEN] = ["Gender", "Number", "Person"];
-lazy_static! {
-    static ref ALLOWED_CHARS : HashSet<u8> = HashSet::from_iter(String::from("AZERTYUIOPQSDFGHJKLMWXCVBNazertyuiopqsdfghjklmwxcvbn'àÀâÂäÄéèÈëÉîÎïôöÖ-çÇ").into_bytes());
-}
+const ALLOWED_CHARS : &'static str = "AZERTYUIOPQSDFGHJKLMWXCVBNazertyuiopqsdfghjklmwxcvbn'àÀâÂäÄéèÈëÉîÎïôöÖ-çÇ";
+type Letters = Vec<u8>;
+
 
 fn find_or_insert<T> (list: &mut Vec<T>, elem: T) -> usize 
         where T: Clone + PartialOrd
@@ -21,6 +24,21 @@ fn find_or_insert<T> (list: &mut Vec<T>, elem: T) -> usize
         None => {
             list.push(elem.clone());
             list.len()
+        }
+    }
+}
+
+
+fn remove_elems<T>(original: &mut Vec<T>, matched_word: &Vec<T>) where T: PartialOrd {
+    let lengths = (original.len(), matched_word.len()); // pool, searched
+    let mut indexes = (0, 0); // pool, searched
+    while indexes.0 < lengths.0 && indexes.1 < lengths.1 {
+        if matched_word[indexes.1] > original[indexes.0] { 
+            indexes.0 += 1;
+        }
+        else if matched_word[indexes.1] == original[indexes.0] {
+            original.remove(indexes.0);
+            indexes.1 += 1; 
         }
     }
 }
@@ -41,56 +59,81 @@ struct Word<'a> {
     morph: [u8; MORPH_TYPES_LEN],
 }
 
+// impl fmt::Display for Word<'_> {
+//     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+//         write!(f, "{}", str::from_utf8(&self.letters).unwrap())
+//     }
+// }
+
+
 struct Index<'a> {
+    // letters: Letters,
+    char_mapping: HashMap<char, u8>,
+    chars: Vec<char>,
     pos_tags: Vec<String>,
     morph_tags: [Vec<String>; MORPH_TYPES_LEN],
     word_defs: Vec<Word<'a>>,
 }
 
 impl<'a> Index<'a> {
-    fn new(word_letters: &'a mut Vec<u8>) -> Index<'a> {
-        let mut morph_tags : [Vec<String>; MORPH_TYPES_LEN] = Default::default();
-        let mut word_defs = vec![];
-        let mut pos_tags : Vec<String> = vec![];
-        let mut ranges = vec![];
+    fn new() -> (Index<'a>, Vec<usize>, Letters) {
+        let mut index = Index {
+            char_mapping: HashMap::new(),
+            chars: vec![],
+            morph_tags: Default::default(),
+            word_defs: vec![],
+            pos_tags: vec![],
+        };
+        ALLOWED_CHARS.chars().for_each(|c| {
+            index.char_mapping.insert(c, index.chars.len() as u8);
+            index.chars.push(c);
+        });
+        let mut letters = vec![];
         let lines = read_lines("data/words.jsonl").expect("Words file not found");
+        let mut ranges = vec![];
         for line in lines {
             if let Ok(word_def) = line {
                 let word_def : Value = serde_json::from_str(&word_def).unwrap();
                 let word = word_def["word"].as_str().unwrap();
                 let pos_tag = word_def["pos"].as_str().unwrap();
-                let pos_tag_index = find_or_insert(&mut pos_tags, String::from(pos_tag));
-                let morphology = Index::get_morph_tags(&mut morph_tags, word_def["morph"].as_object().unwrap());
-                let len_words = word_letters.len();
+                let pos_tag_index = find_or_insert(&mut index.pos_tags, String::from(pos_tag));
+                let morphology = Index::get_morph_tags(&mut index.morph_tags, word_def["morph"].as_object().unwrap());
+                let len_words = letters.len();
                 ranges.push(len_words);
-                if !word.as_bytes().iter().all(|x| ALLOWED_CHARS.contains(x)) {
+                if !word.chars().all(|x| index.char_mapping.contains_key(&x)) {
                     println!("{} not in character set: skipping", word);
                     continue;
                 }
-                word_letters.extend_from_slice(word.as_bytes());
+                letters.extend_from_slice(&index.str_to_u8(word));
                 let new_word_def = Word {
                     letters: &[],
                     pos: pos_tag_index as u8,
                     lol_score: 0,
                     morph: morphology,
                 };
-                word_defs.push(new_word_def);
+                index.word_defs.push(new_word_def);
             }
         }
-        let mut prev_start = 0;
-        for (def, end_index) in word_defs.iter_mut().zip(ranges) {
-            def.letters = &word_letters[prev_start..end_index];
-            prev_start = end_index;
-            // println!("{:?}, {}", def, end_index);
-        }
+        (index, ranges, letters)
+    }
 
-        Index {
-            morph_tags: morph_tags,
-            word_defs: word_defs,
-            pos_tags: pos_tags,
-        }
+    fn str_to_u8(&self, string: &str) -> Vec<u8> {
+        string.chars().map(|c| *self.char_mapping.get(&c).unwrap()).collect()
+    }
+
+    fn u8_to_str(&self, indexes: &[u8]) -> String {
+        indexes.iter().map(|&c| self.chars[c as usize]).collect()
     }
     
+    // associate words to their slices
+    fn associate(&mut self, letters: &'a Letters, ranges: Vec<usize>) {
+        let mut prev_start = 0;
+        for (def, end_index) in self.word_defs.iter_mut().zip(ranges) {
+            def.letters = &letters[prev_start..end_index];
+            prev_start = end_index;
+        }
+    }
+
     fn get_morph_tags(morph_tags: &mut [Vec<String>; MORPH_TYPES_LEN], morph: &Map<String, Value>) -> [u8; 3] {
         let mut tags : [u8; MORPH_TYPES_LEN] = [0; MORPH_TYPES_LEN];
         MORPH_TYPES.iter().enumerate().for_each(|(index, morph_type)| {
@@ -103,14 +146,108 @@ impl<'a> Index<'a> {
         tags
     }
 
+    fn check_contains_all_letters(letter_pool: &[u8], searched: &[u8]) -> bool {
+        let lengths = (letter_pool.len(), searched.len()); // pool, searched
+        if lengths.1 > lengths.0 { return false; }
+        let mut indexes = (0, 0); // pool, searched
+        while indexes.0 < lengths.0 && indexes.1 < lengths.1 {
+            if searched[indexes.1] < letter_pool[indexes.0] { return false; }
+            if searched[indexes.1] > letter_pool[indexes.0] { indexes.0 += 1; }
+            else if searched[indexes.1] == letter_pool[indexes.0] {
+                indexes.0 += 1; indexes.1 += 1; 
+            }
+        }
+        indexes.1 == lengths.1
+    }
+
+    fn new_vec_removed_letters(original: &Letters, matched_word: &Letters) -> Letters {
+        let lengths = (original.len(), matched_word.len()); // pool, searched
+        let mut remaining : Vec<u8> = Vec::with_capacity(lengths.0 - lengths.1);
+        let mut indexes = (0, 0); // pool, searched
+        while indexes.0 < lengths.0 {
+            if indexes.1 == lengths.1 || matched_word[indexes.1] > original[indexes.0] { 
+                remaining.push(original[indexes.0]); 
+                indexes.0 += 1;
+            }
+            else if matched_word[indexes.1] == original[indexes.0] {
+                indexes.0 += 1; indexes.1 += 1; 
+            }
+        }
+        remaining
+    }
+
+
+    fn find_anagrams(&self, mut input: String) {
+        let before = Instant::now();
+
+        input.retain(|c| !c.is_whitespace());
+        let mut sorted_letters : Vec<u8> = input.drain(..).map(|c| *self.char_mapping.get(&c).unwrap()).collect();
+        sorted_letters.sort();
+        let mut candidates : Vec<Matching> = vec![];
+        let mut sorted_letters_cand : Vec<u8> = Vec::with_capacity(100);
+        for word in &self.word_defs {
+            sorted_letters_cand.extend_from_slice(&word.letters);
+            sorted_letters_cand.sort();
+            let candidates_len = candidates.len();
+            for candidate_index in 0..candidates_len {
+                let candidate = &mut candidates[candidate_index];
+                if candidate.letter_pool.len() == 0 { continue; }
+                let check = Index::check_contains_all_letters(&candidate.letter_pool, &sorted_letters_cand);
+                if check {
+                    // remove letters from original pool
+                    remove_elems(&mut candidate.letter_pool,  &sorted_letters_cand);
+                    candidate.matched.push(&word);
+                }
+                
+            }
+            // add new candidate if match 
+            if Index::check_contains_all_letters(&sorted_letters, &sorted_letters_cand) {
+                // remove letters from original pool
+                let remaining_letters = Index::new_vec_removed_letters(&sorted_letters, &sorted_letters_cand);
+                let new_candidate = Matching { letter_pool: remaining_letters, matched: vec![word]};
+                candidates.push(new_candidate);
+            }
+            sorted_letters_cand.clear();
+        }
+        println!("{} candidate group", candidates.len());
+        for cand in &candidates {
+            if cand.letter_pool.len() == 0 {
+                self.print_matching(cand);
+            }
+        }
+        println!("Elapsed time: {:.2?}", before.elapsed());
+    }
+
+    fn print_matching(&self, matching: &Matching) {
+        if matching.letter_pool.len() > 0 {
+            println!("Remaining letters: {}", self.u8_to_str(&matching.letter_pool));
+        }
+        println!("matched {} words:", matching.matched.len());
+        for word in &matching.matched {
+            println!("{} ", self.u8_to_str(word.letters));
+        }
+    }
+
     
 }
 
+#[derive(Debug)]
+struct Matching<'a> {
+    letter_pool: Letters,
+    matched: Vec<&'a Word<'a>>
+}
+
 fn main() {
-    let mut word_letters = vec![];
-    let index = Index::new(&mut word_letters);
-    println!("Indexing over, {} letters", word_letters.len());
+    let (index, ranges, letters) = Index::new();
+    let mut index = index;
+    index.associate(&letters, ranges);
+    println!("Indexing over, {} letters, {} words", letters.len(), index.word_defs.len());
+    // println!("Indexing over, {} letters, {} words", index.letters.len(), index.word_defs.len());
     loop {
 
+        let mut sentence = String::new();
+        println!("Please enter a sentence.");
+        io::stdin().read_line(&mut sentence).expect("Failed to read input");
+        index.find_anagrams(sentence);
     }
 }
